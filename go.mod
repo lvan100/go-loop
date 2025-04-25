c0167fa8c2ccdabd3fe8674330bed11031dba9b6
lvan100 <lvan100@yeah.net>
2025-04-25 10:25:58 +0800

    init first version

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/lvan100/go-loop
+
+go 1.24
+
+require github.com/lvan100/go-assert v0.0.1
+
+require go.uber.org/mock v0.5.1 // indirect
---
+++ loop_test.go
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2025 The Go-Spring Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package loop_test
+
+import (
+	"testing"
+
+	"github.com/lvan100/go-assert"
+	"github.com/lvan100/go-loop"
+)
+
+func fibonacci(n int) int {
+	if n <= 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+}
+
+func BenchmarkRanges(b *testing.B) {
+	const N = 5
+
+	b.Run("for", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			fibonacci(N)
+		}
+	})
+
+	b.Run("loop", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			loop.Times(5, func(i int) {
+				fibonacci(N)
+			})
+		}
+	})
+}
+
+func TestTimes(t *testing.T) {
+	var arr []int
+	loop.Times(5, func(i int) {
+		arr = append(arr, i)
+	})
+	assert.Equal(t, arr, []int{0, 1, 2, 3, 4})
+}
+
+func TestRanges(t *testing.T) {
+	var arr []int
+	loop.Ranges(1, 5, func(i int) {
+		arr = append(arr, i)
+	})
+	assert.Equal(t, arr, []int{1, 2, 3, 4})
+	arr = nil
+	loop.Ranges(5, 1, func(i int) {
+		arr = append(arr, i)
+	})
+	assert.Equal(t, arr, []int{5, 4, 3, 2})
+}
+
+func TestStepRanges(t *testing.T) {
+	var arr []int
+	loop.StepRanges(1, 5, 2, func(i int) {
+		arr = append(arr, i)
+	})
+	assert.Equal(t, arr, []int{1, 3})
+	arr = nil
+	loop.StepRanges(5, 1, -2, func(i int) {
+		arr = append(arr, i)
+	})
+	assert.Equal(t, arr, []int{5, 3})
+}
